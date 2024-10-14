4f9906fc1e9c0a825c84691f531be69f0cfbff54
Andrea Fernández <andreafernez@estudiantec.cr>
2024-10-13 19:28:24 -0600

    tutorial 1 update

--- Tutorial1/hello.ts
+++ Tutorial1/hello.ts
@@ -1,3 +1,9 @@
-console.log("¡Hola, Deno!");
-
+console.log("%cHola Deno", "color: green");
+console.log("%cHola Deno", "background-color: blue");
+console.log("%cHola Deno", "text-decoration: underline");
+console.log("%cHola Deno", "text-decoration: line-through");
+console.log("%cHola Deno", "font-weight: bold");
+console.log("%cHola Deno", "font-style: italic");
+console.log("%cHola %cDeno", "color: green", "color: blue");
+console.log("%cHola %cDeno", "color: 000000", "color: FFFFFF");
 // deno run hello.ts
